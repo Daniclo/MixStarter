ad8c6d6f1c0470c345794eb5d9e77b5d6fbcaa60
DaniColl <intership@ggtech.gg>
2024-05-15 21:53:13 +0200

    Comienzo del diseño de vistas.

--- src/main/java/org/daniclo/mixstarter/MixstarterApplication.java
+++ src/main/java/org/daniclo/mixstarter/MixstarterApplication.java
@@ -3,6 +3,7 @@ package org.daniclo.mixstarter;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,12 @@ import java.io.IOException;
 public class MixstarterApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MixstarterApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MixstarterApplication.class.getResource("fxml/mixstarter.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        stage.setMaximized(true);
+        Image image = new Image(getClass().getResourceAsStream("icons/MixLogo.png"));
+        stage.getIcons().add(image);
+        stage.setTitle("MixStarter");
         stage.setScene(scene);
         stage.show();
     }
